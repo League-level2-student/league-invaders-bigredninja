00de91a8c840f4853badc0f15814a5a71b9599d9
chase <chase@HDCPC2>
2021-01-27 19:31:15 -0800

    fjklkgfg

---
+++ src/GamePanel.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+	@Override
+	public void paintComponent(Graphics g){
+		g.fillRect(10, 10, 100, 100);
+	}
+
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,24 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
 
+public static int WIDTH = 500;
+public static int HEIGHT = 800;
+JFrame frame;
+GamePanel panel;
+
+LeagueInvaders(){
+	frame = new JFrame();
+	panel = new GamePanel();
+}
+void setup() {
+	frame.add(panel);
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+public static void main(String[] args) {
+	LeagueInvaders invaders = new LeagueInvaders();
+	invaders.setup();	
+}
 }
