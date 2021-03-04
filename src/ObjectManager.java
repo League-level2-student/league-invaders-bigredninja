2092b2e7f9d844ffe242d4d6ad050e259b3e7c6d
chase <chase@HDCPC2>
2021-03-03 19:27:42 -0800

    sdfghjkl

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,19 +1,52 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 public class ObjectManager {
-	public void addProjectile(Projectile projectile ) {
-		projectiles.add(projectile);
-		
-		
-		
-		
-		
-		
-	}
+	Random rnd = new Random();
+	ArrayList<Alien> aliens;
+	
 	ArrayList<Projectile> projectiles;
 	Rocketship spaceship;
 	public ObjectManager( Rocketship spaceship) {
 		this.spaceship = spaceship;
 		projectiles = new ArrayList<Projectile>();	
+		aliens = new ArrayList<Alien>();
+}
+	public void addProjectile(Projectile projectile ) {
+		projectiles.add(projectile);
+	}
+	public void addAlien() {
+		aliens.add(new Alien(rnd.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	void update() {
+for (Iterator iterator = aliens.iterator(); iterator.hasNext();) {
+	Alien alien = (Alien) iterator.next();
+	alien.update();	
+	if (alien.y > LeagueInvaders.HEIGHT) {
+		alien.isActive = false;
+	}
+	
+}
+for (Iterator iterator = projectiles.iterator(); iterator.hasNext();) {
+	Projectile projectile = (Projectile) iterator.next();
+	projectile.update();
+	if (projectile.y < 0) {
+		projectile.isActive = false;
+	}
+}
+	}
+	void draw(Graphics g) {
+		spaceship.draw(g);
+		for (Iterator iterator = aliens.iterator(); iterator.hasNext();) {
+			Alien alien = (Alien) iterator.next();
+			alien.draw(g);
+			
+		}
+		for (Iterator iterator = projectiles.iterator(); iterator.hasNext();) {
+			Projectile projectile = (Projectile) iterator.next();
+			projectile.draw(g);
+	}
 }
 }
